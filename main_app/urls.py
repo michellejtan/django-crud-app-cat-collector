3132d26f0e07d64e2bfd5622753df5cad84585d1
michellejtan <michelle0223@gmail.com>
2025-04-24 01:55:25 -0700

    wip: add about, home, cat index, cat detail, create, update, delete, & feeding path

---
+++ main_app/urls.py
@@ -0,0 +1,21 @@
+# from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    # path('create/ ...)
+    #  new path, put forward slash at the end
+    # route for cats index
+    path('cats/', views.cat_index, name='cat-index'),
+    path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
+    path('cats/create/', views.CatCreate.as_view(), name='cat-create'),
+    path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cat-update'),
+    path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cat-delete'),
+    path(
+        'cats/<int:cat_id>/add-feeding/', 
+        views.add_feeding, 
+        name='add-feeding'
+    ),
+]
