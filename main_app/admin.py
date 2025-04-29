a594bf9d66402652445a9fdf4384993aab44058d
michellejtan <michelle0223@gmail.com>
2025-04-29 15:01:21 -0700

    wip: import and register the Toy model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Cat, Feeding
+from .models import Cat, Feeding, Toy
 
-admin.site.register([Cat, Feeding]) # NOT admin.site.register(Cat, Feeding)bc skip the custom ModelAdmin part aka like Tries to use Feeding (a model) as a ModelAdmin class
+admin.site.register([Cat, Feeding, Toy]) # NOT admin.site.register(Cat, Feeding)bc skip the custom ModelAdmin part aka like Tries to use Feeding (a model) as a ModelAdmin class
