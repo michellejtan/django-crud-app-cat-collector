80acd81ef579d92b91c4e048a45f14e061b3384a
michellejtan <michelle0223@gmail.com>
2025-04-30 16:56:16 -0700

    wip: Add a Toy Model & Add a ManyToManyField on one side of the relationship (Cat)

--- main_app/models.py
+++ main_app/models.py
@@ -8,12 +8,27 @@ MEALS = (
     ('D', 'Dinner')
 )
 
+# Add the Toy model
+# If you don’t have your Toy model above your Cat model in models.py, 
+# you’ll need to move it there now. This will correct any toy not defined server errors.
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toy-detail', kwargs={'pk': self.id})
+
 # Create your models here.
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    # Add the M:M relationship
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return self.name
